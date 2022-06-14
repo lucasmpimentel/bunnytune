0c693855c11cafec924e916963500f3d8db4f8be
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-06-14 03:01:58 -0300

    [feat] SplashScreen

---
+++ src/components/shared/BlackButton.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const BlackButton = styled.button`
+  align-items: center;
+  background-color: black;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  display: flex;
+  font-family: Roboto;
+  font-size: 13px;
+  font-weight: 900;
+  height: 55px;
+  justify-content: center;
+  letter-spacing: 0.04em;
+  line-height: 15px;
+  text-transform: uppercase;
+  width: 48%;
+`;
+
+export default BlackButton;
