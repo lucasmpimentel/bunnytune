5e1196593fa353cc1d8a9962afe23b58d4282f45
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-06-21 02:13:58 -0300

    chore: making some changes on sync page

--- src/pages/auth/SpotifyLogin/index.jsx
+++ src/pages/auth/SpotifyLogin/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+/* eslint-disable react/jsx-max-depth */
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { TbArrowBackUp } from 'react-icons/tb';
+import { spotifyLogin } from '../../../service/backEndConnection';
 import BackButton from '../../../components/shared/BackButton';
+import Loading from '../../../components/Loading';
+import * as C from './styles';
+import Context from '../../../context/context';
+import spotfyLogo from '../../../assets/images/Spotify_Logo_RGB_White.png';
 
 export default function SpotifyLogin() {
   const navigate = useNavigate();
+  const { user, loading, setLoading } = useContext(Context);
   const clientId = '111f447098364f91bebc25a748808f71';
-  const redirectUri = 'http://localhost:3000';
+  const redirectUri = 'http://localhost:3000/discovery';
   const AUTH_URL = (
     'https://accounts.spotify.com/authorize?'
     + `client_id=${clientId}&`
@@ -20,20 +27,55 @@ export default function SpotifyLogin() {
     + 'playlist-read-private%20playlist-modify-private'
   );
 
+  const code = new URLSearchParams(window.location.search).get('code');
+
+  const handleClick = () => {
+    window.location.replace(AUTH_URL);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    if (code) {
+      spotifyLogin(code);
+      navigate('/discovery');
+    }
+    const onPageLoad = () => {
+      setLoading(false);
+    };
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad);
+    }
+    return () => window.removeEventListener('load', onPageLoad);
+  }, []);
+
   return (
-    <main>
-      <BackButton type="button" onClick={ () => navigate('/') }>
-        <TbArrowBackUp />
-      </BackButton>
-      <h1>Atenção</h1>
-      <section>
-        <p>
-          Para utilizar a aplicação é necessário sincronizar com a sua conta do Spotify
-        </p>
-        <a href={ AUTH_URL }>
-          <button type="button">Sincronizar</button>
-        </a>
-      </section>
-    </main>
+    loading ? <Loading /> : (
+      <C.Main>
+        <C.Glass>
+          <BackButton type="button" onClick={ () => navigate('/') }>
+            <TbArrowBackUp />
+          </BackButton>
+          { user ? (
+            <C.Title>
+              {`Olá ${user.name}`}
+            </C.Title>
+          ) : <C.Title>Olá</C.Title> }
+          <C.Section>
+            <div>
+              <C.Title>Atenção</C.Title>
+              <C.Text>
+                {'Para utilizar a aplicação é necessário sincronizar '
+                + 'com a sua conta do Spotify'}
+              </C.Text>
+            </div>
+            <C.SpotifyButton onClick={ handleClick } type="button">
+              <C.SpotifyLogo src={ spotfyLogo } alt="sincronizar com spotify" />
+            </C.SpotifyButton>
+          </C.Section>
+        </C.Glass>
+      </C.Main>
+    )
   );
 }
