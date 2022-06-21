9aeb96c41147e173d194a8bc31fe545a7517ce89
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-06-21 02:11:09 -0300

    fix: redirecting back if authorized

--- src/pages/SplashScreen/index.jsx
+++ src/pages/SplashScreen/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import bunnyWhite from '../../assets/images/bunnytuneLogoWhite.svg';
-import * as C from './styles';
 import BlackButton from '../../components/shared/BlackButton';
 import WhiteButton from '../../components/shared/WhiteButton';
+import Context from '../../context/context';
+import * as C from './styles';
 
 export default function SplashScreen() {
   const navigate = useNavigate();
+  const { isAuthorized/* , loading, setLoading */ } = useContext(Context);
+  const GO_BACK = -1;
 
   const handleLogin = () => {
     navigate('login');
@@ -17,25 +20,27 @@ export default function SplashScreen() {
   };
 
   return (
-    <C.Main>
-      <C.Logo
-        src={ bunnyWhite }
-        alt="bunnytune logo"
-      />
-      <C.BtnDivisor>
-        <WhiteButton
-          type="button"
-          onClick={ handleLogin }
-        >
-          LOGIN
-        </WhiteButton>
-        <BlackButton
-          type="button"
-          onClick={ handleSingUp }
-        >
-          CADASTRE-SE
-        </BlackButton>
-      </C.BtnDivisor>
-    </C.Main>
+    !isAuthorized ? (
+      <C.Main>
+        <C.Logo
+          src={ bunnyWhite }
+          alt="bunnytune logo"
+        />
+        <C.BtnDivisor>
+          <WhiteButton
+            type="button"
+            onClick={ handleLogin }
+          >
+            LOGIN
+          </WhiteButton>
+          <BlackButton
+            type="button"
+            onClick={ handleSingUp }
+          >
+            CADASTRE-SE
+          </BlackButton>
+        </C.BtnDivisor>
+      </C.Main>
+    ) : navigate(GO_BACK)
   );
 }
